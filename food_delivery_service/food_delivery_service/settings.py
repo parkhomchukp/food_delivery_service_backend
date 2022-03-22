ee000d026eb82037f8df7b08bfd4262524069aac
Pavlo Parkhomchuk <pavelparkhomchuk@gmail.com>
2022-03-22 12:28:54 +0200

    Installed djoser and drf_simplejwt

--- food_delivery_service/food_delivery_service/settings.py
+++ food_delivery_service/food_delivery_service/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'rest_framework_simplejwt',
     'users'
 ]
 
